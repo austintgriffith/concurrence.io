d665438622691fcb4321e24caafd9c2c3e24eaa2
Austin <austin.griffith@madwiremedia.com>
2017-09-04 16:28:41 -0600

    testing

--- Miner/ipfstest.js
+++ Miner/ipfstest.js
@@ -1,27 +1,45 @@
 const IPFS = require('ipfs')
+const Room = require('ipfs-pubsub-room')
+const ipfs = new IPFS({EXPERIMENTAL:{pubsub: true}})
+
 //npm install --save ipfs
 // (ran that twice)
 //then: npm install --save async
 // Create the IPFS node instance
-const ipfs = new IPFS({EXPERIMENTAL:{pubsub: true}})
 
 
-const topic = 'SOMETOPICTHATISHARDTOGUESSIGUESSIDK'
+
+const topic = 'SOMETOPIC'
 
 console.log("Starting.")
 ipfs.on('ready', () => {
+  ipfs.id((err,id)=>{
+      console.log(id)
+  })
   // Your node is now ready to use \o/
   console.log("Ready.")
+  console.log("Connecting to room....")
+  const room = Room(ipfs,topic)
+  room.on('message', (message) => {
+    console.log(topic+" MESSAGE FROM "+message.from+": "+message.data.toString())
 
+  })
+  setInterval(()=>{
+    room.broadcast("HELLO JABRONIES")
+  },2000)
+/*
   const receiveMsg = (msg) => {
     console.log("MSG:",msg)
     console.log(msg.data.toString())
   }
 
   ipfs.pubsub.subscribe(topic, receiveMsg)
-
+*/
 })
 
+
+
+/*
 setInterval(()=>{
   const msg = new Buffer('JimmyLegs!')
   console.log("Publishing...")
@@ -42,7 +60,7 @@ setInterval(()=>{
     console.log(peerIds)
   })
 },3000)
-
+*/
 
 setInterval(()=>{
   ipfs.swarm.peers(function (err, peerInfos) {
