024f39253c9a2e93941d02dce3111e7be7865667
Austin <austin.griffith@madwiremedia.com>
2017-09-07 09:33:58 -0600

    files

---
+++ Combiner/basic/eventsAddResponse.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+console.log("Reading data...")
+const address = fs.readFileSync(process.argv[2]+".address").toString().trim()
+let abi = false
+if(!address){
+  console.log("Couldn't load "+process.argv[2]+".address")
+}else{
+  abi = JSON.parse(fs.readFileSync(process.argv[2]+".abi"));
+  if(!abi){
+    console.log("Couldn't load "+process.argv[2]+".abi")
+  }else{
+    console.log("Loading...")
+    web3.eth.getAccounts().then((accounts)=>{
+      console.log("Loaded account [1] "+accounts[1])
+      let contract = new web3.eth.Contract(abi,address)
+      console.log("interact...")
+      contract.getPastEvents('AddResponse', {
+          fromBlock: 0,
+          toBlock: 'latest'
+      }, function(error, events){
+        console.log(events);
+      })
+    })
+  }
+}
+
+
+
+
+/*
+let deployed = test.deploy({
+  data: bytecode,
+  arguments: ["CONARG1"]
+})
+console.log("Deployed...")
+
+console.log(deployed)
+*/
