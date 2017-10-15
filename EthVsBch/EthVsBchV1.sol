92d949023bb8421131903c44732a49dc9eb83741
Austin <austin.griffith@madwiremedia.com>
2017-10-15 17:38:09 -0600

    v1 of scripts for clarity

---
+++ EthVsBch/EthVsBchV1.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.11;
+
+/*
+A simple 'request oracle client' that needs to know the price of Eth and Bch
+*/
+
+//simple Store interface with just the function we need
+contract Store{function getPrice(bytes32 _symbol) constant returns (uint) {}}
+
+contract EthVsBch {
+
+    //string to hold source address of oracle
+    address public source;
+
+    function EthVsBch(address _source) {
+      source = _source;
+    }
+
+    //anyone can get any price by symbol
+    function whoIsWinning() constant returns (string,uint) { /*whenNotMigrating*/
+      Store store = Store(source);
+      uint priceOfEth = store.getPrice("ETH");
+      uint priceOfBch = store.getPrice("BCH");
+      if( priceOfEth > priceOfBch ){
+        return ("ETH",priceOfEth);
+      }else if ( priceOfEth < priceOfBch ){
+        return ("BCH",priceOfBch);
+      }else{
+        return ("TIE!",priceOfEth);
+      }
+    }
+}
---
+++ Store/StoreV1.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.11;
+
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import "Predecessor.sol";
+
+contract Store is Ownable,Predecessor {
+
+    //string to hold source url of price information for reference
+    string public source;
+    
+    //prices mapped by SYMBOL => price in USD
+    mapping (bytes32 => uint) price;
+
+    function Store(string _source) {
+      source = _source;
+    }
+
+    //only the owner can set prices by symbol
+    function setPrice(bytes32 _symbol,uint _price) onlyOwner {
+      //setPrice should never get called once a descendant is set
+      assert(descendant==address(0));
+      price[_symbol]=_price;
+    }
+
+    //anyone can get any price by symbol
+    function getPrice(bytes32 _symbol) constant returns (uint) { /*whenNotMigrating*/
+      //if there is a descendant, pass the call on
+      if(descendant!=address(0)) {
+        return Store(descendant).getPrice(_symbol);
+      }
+      return price[_symbol];
+    }
+}
