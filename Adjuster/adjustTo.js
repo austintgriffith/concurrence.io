e5523410318a7359d9c1abbef4d72b37b45a1d63
Ubuntu <ubuntu@ip-172-31-9-222.us-east-2.compute.internal>
2017-10-15 23:06:22 +0000

    inherit work

--- Adjuster/adjustTo.js
+++ Adjuster/adjustTo.js
@@ -1,9 +1,8 @@
 //
 // usage: node contract adjustTo Adjuster null #CONTRACTADDRESS# #TARGET# #ACCOUNTINDEX#
 //
-// ex: node contract adjustTo Adjuster null 0xB216C56621Bc83EFcDd0F10d2De3049Cee97eFB4 128 1
+// ex: node contract adjustTo Adjuster null 0xXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX 128 1
 //
-
 module.exports = (contract,params,args)=>{
   console.log("**== adjusting Simple contract at "+args[5]+" to "+args[6]+" using account "+params.accounts[args[7]])
   return contract.methods.adjustTo(args[5],args[6]).send({
