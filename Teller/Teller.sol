c42099518cfe21e9bf68f13e6007324a709cc579
Austin <austin.griffith@madwiremedia.com>
2017-10-21 08:06:53 -0600

    working on teller

--- Teller/Teller.sol
+++ Teller/Teller.sol
@@ -1,26 +1,15 @@
 pragma solidity ^0.4.11;
 
-//contract ExternalContract{ function __callback(uint _number,string _name) {} }
-
-contract LinkedList {
-  struct Object{ bytes32 next;uint number;bytes32 name;}
-  bytes32 public head;
-  mapping (bytes32 => Object) public objects;
-  function total() public constant returns (uint) {}
-  function getEntry(bytes32 _id) public returns (bytes32,uint,bytes32){}
-}
-
 contract Teller {
 
-  uint16 public quorum = 300;
+  uint16 public quorum = 400;
   bytes32 public winningName;
   uint public winningVotes;
 
   enum States {
         WaitingForQuorum,
         CountingVotes,
-        ElectionFinished,
-        CalledBack
+        ElectionFinished
   }
 
   States public state = States.WaitingForQuorum;
@@ -58,13 +47,17 @@ contract Teller {
       if( currentPointer==0 ){
         state = States.ElectionFinished;
       }
-    }else if( state == States.ElectionFinished ){
-      state = States.CalledBack;
-      //normally you would call back to a contract that needed those results if they
-      //aren't coming here to get the results
-      //contract ExternalContract{ function __callback(uint _number,string _name) {} }
     }else{
       revert();
     }
   }
+}
+
+//simple interface to the LinkedList
+contract LinkedList {
+  struct Object{ bytes32 next;uint number;bytes32 name;}
+  bytes32 public head;
+  mapping (bytes32 => Object) public objects;
+  function total() public constant returns (uint) {}
+  function getEntry(bytes32 _id) public returns (bytes32,uint,bytes32){}
 }
