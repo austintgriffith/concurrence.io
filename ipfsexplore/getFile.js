f429809bc4aa2d0d41236605d0e3ef33267b0fb9
Austin Griffith <austin@pyth.io>
2017-10-26 22:36:00 +0000

    wait longer for ipfs

--- ipfsexplore/getFile.js
+++ ipfsexplore/getFile.js
@@ -1,7 +1,33 @@
 const IPFS = require('ipfs')
 const ipfs = new IPFS()
 const fs = require("fs")
+
+let reallyReady = false;
+
 ipfs.on('ready', () => {
+  setInterval(()=>{
+    ipfs.swarm.peers(function (err, peerInfos) {
+      if (err) {
+        throw err
+      }
+      //console.log(peerInfos.length)
+      if(!reallyReady && peerInfos.length>0){
+        reallyReady=true;
+        console.log("really ready?")
+        setTimeout(ready,30000);
+      }
+    })
+  },1000)
+
+
+
+})
+ipfs.on('error', (err) => {
+  console.log("ipfs error:",err)
+})
+
+
+function ready(){
   let multihashStr = process.argv[2];
   console.log("Getting "+multihashStr)
   ipfs.files.get(multihashStr, function (err, stream) {
@@ -9,7 +35,4 @@ ipfs.on('ready', () => {
       file.content.pipe(process.stdout)
     })
   })
-})
-ipfs.on('error', (err) => {
-  console.log("ipfs error:",err)
-})
+}
