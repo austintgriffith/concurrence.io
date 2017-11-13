f3832f14f5a9664545354a951b982c3cced47546
Austin <austin.griffith@madwiremedia.com>
2017-11-13 06:00:15 -0700

    account js mess around

--- reactapp/src/AccountSelect.js
+++ reactapp/src/AccountSelect.js
@@ -3,38 +3,24 @@ import React, { Component } from 'react';
 import { Select } from 'antd';
 const Option = Select.Option;
 
-function handleChange(value) {
-console.log(`selected ${value}`);
-}
-
-function handleBlur() {
-console.log('blur');
-}
-
-function handleFocus() {
-console.log('focus');
-}
-
-
 class AccountSelect extends Component {
 
   render() {
-    console.log("AS",this.props.concurrence.accounts)
+    let accountOptions = this.props.accounts.map((account)=>{
+      return (
+        <Option key={account} value={account}>{account}</Option>
+      )
+    })
     return (
       <div className="AccountSelect">
         <Select
          showSearch
          style={{ width: 200 }}
-         placeholder="Select a person"
-         optionFilterProp="children"
-         onChange={handleChange}
-         onFocus={handleFocus}
-         onBlur={handleBlur}
+         placeholder="Select an account"
+         onChange={this.props.accountSelect}
          filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
        >
-         <Option value="jack">Jack</Option>
-         <Option value="lucy">Lucy</Option>
-         <Option value="tom">Tom</Option>
+          {accountOptions}
        </Select>
       </div>
     );
--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,35 +1,73 @@
 import React, { Component } from 'react'
 import './App.css'
-import ProviderSelect from './ProviderSelect.js'
 import AccountSelect from './AccountSelect.js'
 var concurrence = require("concurrence")
-
 class App extends Component {
 
-  setProvider(provider) {
-    console.log("Set provider",provider)
+  constructor(props) {
+    super(props);
+    this.state = { accounts:false, selectedAddress:false };
+  }
+
+  componentDidMount() {
+    console.log("Mounting up.")
+    concurrence.init({DEBUG:true,provider:window.web3.currentProvider},(err)=>{
+      console.log(concurrence.version)
+      console.log(concurrence.contracts["Main"].address)
+      this.setState({accounts:concurrence.accounts})
+    });
+  }
+
+  accountSelect(value) {
+    console.log("accountSelect",value)
+    concurrence.selectAccount(value)
+    this.setState({selectedAddress:concurrence.selectedAddress})
+    concurrence.balanceOf(value).then((balance)=>{
+      console.log("balance",balance)
+      this.setState({balance:balance})
+    })
   }
 
   render() {
+    let {accounts,balance} = this.state
 
+    ///////////////////////////account
     let accountSelect = ""
-    if(concurrence.accounts){
+    if(accounts){
       accountSelect = (
-        <AccountSelect concurrence={concurrence} />
+        <AccountSelect accounts={accounts} accountSelect={this.accountSelect.bind(this)} />
+      )
+    }else{
+      accountSelect = (
+        <div>
+          Connecting...
+        </div>
       )
     }
 
+    ///////////////////////////balance
+    let balanceBox = ""
+    if(balance){
+      balanceBox = (
+        <div>
+          {balance}
+        </div>
+      )
+    }else{
+      balanceBox = (
+        <div>
+          Loading...
+        </div>
+      )
+    }
 
+    ////////////////////////////render
     return (
       <div className="App">
-
-        <ProviderSelect concurrence={concurrence} setProvider={this.setProvider}/>
-
         {accountSelect}
-
+        {balanceBox}
       </div>
     );
   }
 }
-
 export default App;
